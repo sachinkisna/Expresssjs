4043550e927c2cfbcdc10dbcdc6cd51b8d308ff5
Sachin Rout <sachinsoftware0001@gmail.com>
2026-04-21 09:02:43 +0530

    updated

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3635;
+const fs= require("fs");
+const path=require("path");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.get('/about', (req, res) => {
-  res.send('Hello About')
-})
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port https://localhost:${port}`)
-})
+});
